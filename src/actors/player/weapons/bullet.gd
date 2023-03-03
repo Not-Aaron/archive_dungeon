0a0707b3a3ab18cb4aa654fe94d2dba658972d33
Ethan <ethanransberger@gmail.com>
2023-03-02 20:48:19 -0600

    update

--- src/actors/player/weapons/bullet.gd
+++ src/actors/player/weapons/bullet.gd
@@ -1,43 +1,31 @@
 extends Area2D
+class_name Bullet
+export (int) var speed = 20
 
-onready var player = $"../Player"
-export var aggro_radius = 500
-export var speed = 200
-var health = 20
-var damage = 10
-var max_health = 20
-var velocity = Vector2.ZERO
+var direction := Vector2.ZERO
 
+onready var kill_timer = $KillTimer
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func _ready():
+	kill_timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# warning-ignore:unused_argument
-func _process(delta: float) -> void:
-	if not is_instance_valid(player) or position.distance_to(player.position) > aggro_radius:
-		velocity = Vector2.ZERO
-		return
-	velocity = speed*position.direction_to(player.position)
-	position += velocity * delta
-
-# Damage player upon collision
-func _on_FlyingEnemy_body_entered(body: Node) -> void:
-	if body == player:
-		if player.vulnerable:
-			player.take_damage(damage)
-	
-func take_damage(damage: float):
-	health -= damage
-	if health <= 0:
-		die()
+func _physics_process(_delta: float):
+	if direction != Vector2.ZERO:
+		var velocity = direction * speed
 		
-func die():
+		global_position += velocity
+
+
+func set_direction(direction: Vector2):
+	self.direction = direction
+	
+
+func _on_KillTimer_timeout():
 	queue_free()
+
+
+func _on_Bullet_body_entered(body):
+	if body.has_method("handle_hit"):
+		body.handle_hit()
+		queue_free()
