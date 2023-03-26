8c747b5b2c40a4f9ae22e375636c0f1aea378ab4
EthanRansberger <100398629+EthanRansberger@users.noreply.github.com>
2023-03-25 23:31:02 -0500

    f
    f

--- src/actors/player/weapons/ranged.gd
+++ src/actors/player/weapons/ranged.gd
@@ -2,19 +2,30 @@ extends Position2D
 export (PackedScene) var Bullet	
 export (PackedScene) var Slice
 onready var attack = Slice
+onready var cooldown = false
 	
 # Called when the node enters the scene tree for the first time.
 func shoot():
-	if $atkcd.time_left() !=0 and $atkcd.timeleft() :
+	#if $atkcd.is_stopped() == false or $atkcd.get_time_left()!=0:
+		#return
+		#$atkcd.start
+#	if $atkcd.get_time_left() !=0 and $atkcd.get_time_left()>=0:
+	#	return
+	#different weapons ahve different cooldowns
+	#if $atkcd.get_time_left()>0:
+		#return
+	if cooldown == false:
+		#return
 		$atkcd.start()
+		
 
-	var bullet_instance = attack.instance()
-	var target = get_global_mouse_position()
-	var direction_to_mouse = global_position.direction_to(target).normalized()
-	bullet_instance.set_direction(direction_to_mouse)
-	get_parent().get_parent().add_child(bullet_instance)
-	bullet_instance.global_position = global_position
-
+		var bullet_instance = attack.instance()
+		var target = get_global_mouse_position()
+		var direction_to_mouse = global_position.direction_to(target).normalized()
+		bullet_instance.set_direction(direction_to_mouse)
+		get_parent().get_parent().add_child(bullet_instance)
+		bullet_instance.global_position = global_position
+		cooldown == true
 func switch():
 	match(attack):
 		Bullet:
@@ -24,4 +35,4 @@ func switch():
 
 
 func _on_atkcd_timeout():
-	pass # Replace with function body.
+	cooldown == false # Replace with function body.
