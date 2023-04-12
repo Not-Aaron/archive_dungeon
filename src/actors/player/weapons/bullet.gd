df00a7e96e78393d194441283203b50053493e6e
EthanRansberger <100398629+EthanRansberger@users.noreply.github.com>
2023-04-11 20:09:21 -0500

    working on ui and swing

--- src/actors/player/weapons/bullet.gd
+++ src/actors/player/weapons/bullet.gd
@@ -2,11 +2,16 @@ extends Area2D
 class_name Bullet
 export (int) var speed = 20
 var damage = 15
-
+var pierce = 0
 var direction := Vector2.ZERO
 var spin = PI
+# crit system, does 2 time damage
+#pierces twice
+export (PackedScene) var critpar
+
 
 onready var kill_timer = $KillTimer
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	kill_timer.start()
@@ -32,6 +37,20 @@ func gettime() -> float:
 	
 func _on_Bullet_area_entered(area: Area2D) -> void:
 	if area.has_method("take_damage"):
-		area.take_damage(damage)
-		queue_free()
-
+		var crit = rand_range(0,4)
+		var totdamage = damage
+		if crit >= 3:
+			totdamage = damage * 2
+			#$crit.visible = true
+			var critical = critpar.instance()
+			critical.global_position = global_position
+			#have crit as its own scene and instance it at that position
+		else:
+			totdamage=damage
+		if pierce >= 1:
+			
+			area.take_damage(totdamage)
+			queue_free()
+		else:
+			area.take_damage(totdamage)
+			pierce+=1
