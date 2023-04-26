6c2289e1b9e7ee7409533787f965a50b1dcc15c0
EthanRansberger <100398629+EthanRansberger@users.noreply.github.com>
2023-04-26 15:37:22 -0500

    //

--- src/actors/scroll enemy/Eye.gd
+++ src/actors/scroll enemy/Eye.gd
@@ -48,15 +48,16 @@ func _on_KillTimer_timeout():
 	#print(rad2deg(dirang))
 	queue_free()
 	
+		
 
-func _on_Bullet_body_entered(body):
+
+
+
+
+func _on_Eye_body_entered(body):
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
 		if body.has_method("take_blind"):
 			body.take_blind()
 		queue_free()
-	
-		
-
-
-
+	 # Replace with function body.
